97c3a2662948a1d299354cd30ec46ca7f59268d3
Rakindu Niwunhella <rakindu15@gmail.com>
2026-02-23 15:16:48 +0530

    Revert "Rv 89 fast api integrations"

--- CheckingIfNewDataAvailable/ee_init.py
+++
@@ -1,9 +0,0 @@
-import ee
-
-SERVICE_ACCOUNT = "gee-lambda@stable-being-427214-c6.iam.gserviceaccount.com"
-KEY_FILE = "gee-key.json"
-
-credentials = ee.ServiceAccountCredentials(SERVICE_ACCOUNT, KEY_FILE)
-ee.Initialize(credentials)
-
-print("Earth Engine initialized successfully")
