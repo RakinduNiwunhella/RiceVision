5ba7d43b32210c12425dddeb4fbef3bc8c316a7f
Binada <binada2005@gmail.com>
2026-02-25 21:47:45 +0530

    RV-80 combined csvs

--- Back-End/Automation/main.py
+++ Back-End/Automation/main.py
@@ -1,11 +1,12 @@
 from Automation.scrapers.dmc_scraper import scrape_dmc_reports
 from Automation.processors.pdf_extractor import process_all_pdfs
-
+from Automation.processors.csv_combiner import combine_csvs
 
 def run_pipeline():
     print("Starting pipeline...")
     scrape_dmc_reports()
     process_all_pdfs()
+    combine_csvs()
     print("Pipeline finished.")
 
 
