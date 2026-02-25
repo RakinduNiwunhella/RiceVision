cdf85189e2d0c5d95aa6d1869ffdba8a6135af54
Binada <binada2005@gmail.com>
2026-02-25 22:23:13 +0530

    RV-80 cleaned dataset

--- Back-End/Automation/config/settings.py
+++ Back-End/Automation/config/settings.py
@@ -2,8 +2,11 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-PDF_DIR = os.path.join(BASE_DIR, "data", "pdfs")
-CSV_DIR = os.path.join(BASE_DIR, "data", "csvs")
-COMBINED_DIR = os.path.join(BASE_DIR, "data", "combined")
+DATA_DIR = os.path.join(BASE_DIR, "data")
+PDF_DIR = os.path.join(DATA_DIR, "pdfs")
+CSV_DIR = os.path.join(DATA_DIR, "csvs")
+FINAL_DIR = os.path.join(DATA_DIR, "final")
 
-DMC_URL = "https://www.dmc.gov.lk/index.php?option=com_dmcreports&view=reports&Itemid=273&limit=0&search=&report_type_id=1&fromdate=2022-01-01&todate=2026-02-18&lang=en"
+os.makedirs(PDF_DIR, exist_ok=True)
+os.makedirs(CSV_DIR, exist_ok=True)
+os.makedirs(FINAL_DIR, exist_ok=True)
