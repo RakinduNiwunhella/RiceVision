a5e6801ed371ad32e065d0de0b17f7d21dfd7943
Rakindu Niwunhella <rakindu15@gmail.com>
2026-03-07 11:43:27 +0530

    RV-142 Updated the health summary fastapi backend

--- Back-End/SDGP/routes/dashboard.py
+++ Back-End/SDGP/routes/dashboard.py
@@ -6,7 +6,7 @@ router = APIRouter()
 @router.get("/yield")
 def get_yield():
     response = supabase.table("yield_forecast_view") \
-        .select("total_yield_tons") \
+        .select("total_yield_kgs") \
         .single() \
         .execute()
 
@@ -27,11 +27,11 @@ def get_best_yield_districts():
 
 @router.get("/health-summary")
 def get_health_summary():
-    response = supabase.table("paddy_health_summary_view") \
+    response = supabase.table("sri_lanka_paddy_health_summary") \
         .select("normal_pct, mild_stress_pct, severe_stress_pct") \
-        .eq("district", "kurunegala") \
         .single() \
         .execute()
+
     return response.data
 
 
